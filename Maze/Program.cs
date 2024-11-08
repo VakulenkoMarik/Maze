cb16c722a2d032da19b531464abdce4b869cebb4
NeuronStud1o <136814357+VakulenkoMarik@users.noreply.github.com>
2024-11-09 00:11:47 +0200

    Main menu (Additional)

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -4,6 +4,7 @@
 
     // Supplement
     static int moves = 10;
+    static string gameExeptionMessage = "";
 
     // Symbols
     static char wall = '#';
@@ -33,6 +34,109 @@
 
     static void Main(string[] args)
     {
+        GoToMainMenu();
+    }
+
+    static void GoToMainMenu()
+    {
+        DrawMenuText();
+        MainMenuInput();
+    }
+
+    static void MainMenuInput()
+    {
+        string? inp = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(inp))
+        {
+            MainMenuInput();
+            return;
+        }
+
+        MainMenuLogic(inp[0]);
+    }
+
+    static void MainMenuLogic(char symbol)
+    {
+        if (symbol == 'a')
+        {
+            CreateNewGame();
+            StartNewGame();
+        }
+        else if (symbol == 'b')
+        {
+            SettingsMenu();
+            GoToMainMenu();
+        }
+        else if (symbol == 'c')
+        {
+            return;
+        }
+        else
+        {
+            GoToMainMenu();
+        }
+    }
+
+    static void SettingsMenu()
+    {
+        Console.Clear();
+
+        int newHeight, newWidth, newblockFreq;
+
+        Console.WriteLine("Enter the height of the field");
+
+        if (!int.TryParse(Console.ReadLine(), out newHeight) || newHeight <= 3 || newHeight > 100)
+        {
+            gameExeptionMessage = "Height error";
+            return;
+        }
+
+        Console.WriteLine("Enter the width of the field");
+
+        if (!int.TryParse(Console.ReadLine(), out newWidth) || newWidth <= 4 || newWidth > 50)
+        {
+            gameExeptionMessage = "Width error";
+            return;
+        }
+
+        Console.WriteLine("Enter the frequency of the wall appearance");
+
+        if (!int.TryParse(Console.ReadLine(), out newblockFreq) || newblockFreq <= 0 || newblockFreq >= 90)
+        {
+            gameExeptionMessage = "Frequency error";
+            return;
+        }
+
+        gameExeptionMessage = "";
+
+        (height, width, blockFreq) = (newHeight, newWidth, newblockFreq);
+    }
+
+    static void DrawMenuText()
+    {
+        Console.Clear();
+
+        Console.WriteLine(gameExeptionMessage);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("-\u0E4F MAZE \u0E4F- \n");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("- Play (a)");
+        Console.WriteLine("- Settings (b)");
+        Console.WriteLine("- Exit (c)");
+    }
+
+    static void CreateNewGame()
+    {
+        reachedFinish = false;
+        jetpackElevated = false;
+        moves = 10;
+    }
+
+    static void StartNewGame()
+    {
         Generate();
         Draw();
 
@@ -43,7 +147,7 @@
             Draw();
         }
         
-        Console.Write("The end");
+        GoToMainMenu();
     }
 
     static void GenerateField()
@@ -81,8 +185,10 @@
     static void Draw()
     {
         Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Moves: " + moves);
 
+        Console.ForegroundColor = ConsoleColor.White;
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
